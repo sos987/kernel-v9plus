158ecd418a360631c766fd2a599b3a4339be3b29
sos987@gmail.com <sos987@gmail.com>
2012-02-23 22:49:53 +0300

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Fri Feb 17 10:15:22 MSK 2012"
+#define LINUX_COMPILE_TIME "10:15:22"
+#define LINUX_COMPILE_BY "sos987"
+#define LINUX_COMPILE_HOST "sos987-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.1 (Sourcery CodeBench Lite 2011.09-69) "
